73b64ba2df0bee1da203c2f1e93d0f56c64028fa
nick <nikhilkubde21@gmail.com>
2021-04-24 02:55:29 +0530

    UI Part of Keep is Completed

--- src/Pages/Keep/KeepNotes.jsx
+++ src/Pages/Keep/KeepNotes.jsx
@@ -1,31 +1,50 @@
-import React, { useState } from 'react';
-import './addnotes.css'
+import React, { useState } from "react";
+import "./addnotes.css";
 import Button from "@material-ui/core/Button";
-import Form from 'react-bootstrap/Form';
-import AddIcon from '@material-ui/icons/Add';   
+import Form from "react-bootstrap/Form";
+import AddIcon from "@material-ui/icons/Add";
 
-const KeepNote = () =>
-{
-    const [titled, setTitled] = useState();
+const KeepNote = () => {
+    const [note, setNote] = useState(
+        {
+            title: "",
+            content: "",
+        }
+    );
 
-    const Title_in = (events) =>
-    {
-        setTitled(events.target.value);
+    const InputData = (event) => {
+        const { value, name } = event.target;
+        setNote((prevData) => {
+            return {
+                ...prevData,
+                [name]: value
+            };
+        });
+        console.log(note);
     }
 
-    return(
+
+    return (
         <>
             <main>
-                <div className="container">
-                    <form>      
-                        <input 
-                        type='text' 
-                        placeholder="Title" 
-                        autoComplete="off"
-                        onChange={Title_in}
+                <div className='container'>
+                    <form>
+                        <input
+                            type='text'
+                            name="title"
+                            value={ note.title}
+                            onChange={InputData}
+                            placeholder='Title'
+                            autoComplete='off'
                         />
-                        <textarea column="" row="" placeholder="Enter the notes here!!!" />
-                        <Button variant="contained" color="disabled"  >
+                        <textarea column=''
+                            row=''
+                            name="content"
+                            value={note.content }
+                            onChange={InputData }
+                            placeholder='Enter the notes here!!!'
+                        />
+                        <Button variant='contained' color='disabled'>
                             <AddIcon />
                         </Button>
                     </form>
@@ -33,6 +52,6 @@ const KeepNote = () =>
             </main>
         </>
     );
-}
+};
 
 export default KeepNote;
--- src/Pages/TODO_material/ToDo_Material.jsx
+++ src/Pages/TODO_material/ToDo_Material.jsx
@@ -4,57 +4,52 @@ import AddIcon from '@material-ui/icons/Add';
 import DeleteIcon from '@material-ui/icons/Delete';
 import ListTodo from './list_component';
 
-const TodoMaterial = () =>
-{
+const TodoMaterial = () => {
     const [list, setList] = useState();
     const [item, setItem] = useState([]);
 
-    const inValue = (events) =>
-    {
-        return(
+    const inValue = (events) => {
+        return (
             setList(events.target.value)
         );
     }
 
     const butnPress = () => {
-            setItem((arra) => {
-                return[...arra,list]
-            }
-            );
-            setList("")
+        setItem((arra) => {
+            return [...arra, list]
         }
+        );
+        setList("")
+    }
 
-        const DeItem = (id) =>
-        {
-            setItem((itemDel) => {
-                return itemDel.filter((arrDel, index) => 
-                {
-                   return index !== id; 
-                }
-                )
+    const DeItem = (id) => {
+        setItem((itemDel) => {
+            return itemDel.filter((arrDel, index) => {
+                return index !== id;
             }
-            );
+            )
         }
+        );
+    }
 
-    return(
+    return (
         <>
             <div className="main_div">
                 <div className="center_div">
                     <br />
-                        <h1>TODO LIST</h1>
+                    <h1>TODO LIST</h1>
                     <br />
-                        <input type="text" value={list} onChange={inValue} placeholder="Enter The Task" />
-                        <AddIcon className="newBtn" onClick={butnPress} />
+                    <input type="text" value={list} onChange={inValue} placeholder="Enter The Task" />
+                    <AddIcon className="newBtn" onClick={butnPress} />
                     <br />
-                        <ol>
-                            {item.map( (val, index) => 
-                            {
-                                return (
-                                    <ListTodo texert={val} key={index} id={index} deleteList={DeItem}/>
-                                ); 
-                            })   
-                            }
-                        </ol>
+                    <ol>
+                        {item.map((val, index) => {
+                            return (
+                                <ListTodo texert={val} key={index} id={index} deleteList={DeItem} />
+                            );
+                        })
+                        }
+                    </ol>
                     <br />
                 </div>
             </div>
